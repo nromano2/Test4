bf046d75899cf09882e1c5165a9abbc3892163b7
Nick Romano <89424910+nromano2@users.noreply.github.com>
2023-01-11 13:27:05 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,21 @@
+/**
+ *  This program is just a simple Hello World program that displays the line "Hello World".
+ *  I'm running this program just as a way to setup my GitHub account and a repository for CS131.
+ */
+
+/**
+ * @author nicholasromano
+ * @version 1.0
+ * GitHub Repository Setup
+ * Spring 2023
+ */
+public class Application {
+
+	/**
+	 * Main Method displays Hello World! to the console
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
